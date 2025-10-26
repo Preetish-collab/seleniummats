0c696da401c2ea66d39496bd9306dab701f1f09a
Preetish-collab <preetishkumarb@gmail.com>
2025-10-26 18:31:40 +0530

    october 25 script for Webtable and BT

---
+++ src/main/java/org/project/BootstrapDD.java
@@ -0,0 +1,41 @@
+package org.project;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.List;
+
+public class BootstrapDD {
+    public static WebDriver driver;
+    public static void main(String[] args) throws InterruptedException {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.navigate().to("http://seleniumpractise.blogspot.in/2016/08/bootstrap-dropdown-example-for-selenium.html");
+        // First we have to click on menu item then only dropdown items will display
+        driver.findElement(By.xpath(".//*[@id='menu1']")).click();
+        // adding 2 seconds wait to avoid Sync issue
+        Thread.sleep(2000);
+        // Dropdown items are coming in <a> tag so below xpath will get all
+        // elements and findElements will return list of WebElements
+        List<WebElement> list = driver.findElements(By.xpath("//ul[@class='dropdown-menu']//li/a"));
+        // We are using enhanced for loop to get the elements
+        for (WebElement ele : list)
+        {
+            // for every elements it will print the name using innerHTML
+            System.out.println("Values " + ele.getAttribute("innerHTML"));
+            // Here we will verify if link (item) is equal to Java Script
+            if (ele.getAttribute("innerHTML").contains("JavaScript")) {
+                // if yes then click on link (iteam)
+                ele.click();
+                // break the loop or come out of loop
+                break;
+
+            }
+
+        }
+    }
+}
