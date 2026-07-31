9daec36112641a92f3e127f9eda7829741e4d417
Preetish-collab <preetishkumarb@gmail.com>
2026-07-31 10:59:45 +0530

    Add files via upload
    playwright files

--- playwright.config.js
+++ playwright.config.js
@@ -1,5 +1,6 @@
 // @ts-check
 import { defineConfig, devices } from '@playwright/test';
+import { off } from 'node:cluster';
 
 /**
  * Read environment variables from file.
@@ -42,7 +43,7 @@ export default defineConfig({
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
-    screenshot: 'on',
+    screenshot:'on-first-failure',
     video: 'retain-on-failure',
   },
 
