df2932ece5fee03cea095c582b5ea82ef28908a0
Preetish-collab <preetishkumarb@gmail.com>
2026-08-01 13:38:41 +0530

    Delete src/main/java/seleniumprac/SelectionClass.java

--- src/main/java/seleniumprac/SelectionClass.java
+++
@@ -1,9 +0,0 @@
-package seleniumprac;
-
-import io.github.bonigarcia.wdm.WebDriverManager;
-
-public class SelectionClass {
-    public static void main(String[] args) {
-        WebDriverManager.chromedriver().setup();
-    }
-}
